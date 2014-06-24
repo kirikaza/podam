d8fb6233af9edbd44093f61d987b288b82767aec
Daniil Ivanov <daniil.ivanov@gmail.com>
2014-06-24 23:38:00 +0300

    Test RandomDataProviderStrategy with variable collection and map names

---
+++ src/test/java/uk/co/jemos/podam/test/strategies/CustomRandomDataProviderStrategy.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package uk.co.jemos.podam.test.strategies;
+
+import uk.co.jemos.podam.api.AbstractRandomDataProviderStrategy;
+
+/**
+ * Random data provider strategy with variable map and collection length
+ * 
+ * @author divanov
+ * 
+ */
+public class CustomRandomDataProviderStrategy extends AbstractRandomDataProviderStrategy {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getNumberOfCollectionElements(Class<?> type) {
+
+		int number;
+		if (String[].class.isAssignableFrom(type)) {
+			number = 2;
+		} else if (Boolean.class.isAssignableFrom(type)) {
+			number = 3;
+		} else if (Long.class.isAssignableFrom(type)) {
+			number = 4;
+		} else if (Integer.class.isAssignableFrom(type)) {
+			number = 5;
+		} else {
+			number = super.getNumberOfCollectionElements(type);
+		}
+		return number;
+	}
+}
